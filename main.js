431e735f0389216e93585130065cc8889f876c44
DeSen Xu <2237803016@qq.com>
2023-03-05 12:11:39 +0800

    update: 删除了多余的语言文件

--- main.js
+++ main.js
@@ -26,12 +26,12 @@ function createWindow () {
   })
 }
 
+app.commandLine.appendSwitch('lang', 'zh-CN')
 // 这段程序将会在 Electron 结束初始化
 // 和创建浏览器窗口的时候调用
 // 部分 API 在 ready 事件触发后才能使用。
 app.whenReady().then(() => {
   createWindow()
-
   app.on('activate', function () {
     // 通常在 macOS 上，当点击 dock 中的应用程序图标时，如果没有其他
     // 打开的窗口，那么程序会重新创建一个窗口。
